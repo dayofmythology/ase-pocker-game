a0a493398c7b1cbd919f893e7b6e66120f09c97b
Prakash Nath Jha <prakash.nath.jha@sap.com>
2023-01-23 21:19:29 +0530

    feat: add state as private member variable to table service class

--- test/table-service.test.js
+++ test/table-service.test.js
@@ -1,29 +1,38 @@
-import { assert, expect } from 'chai' // CHOICE: use assert or expect
-import { TableService, Player } from '../index.js'
+import State from "../lib/state.js";
+import { assert, expect } from "chai"; // CHOICE: use assert or expect
+import { TableService, Player } from "../index.js";
 
 // TODO: implement proper unit tests
-describe('TableService', function () {
-  let tableService = null
+describe("TableService", function () {
+  let tableService = null;
 
   this.beforeEach(function () {
-    tableService = new TableService(/* options */)
-  })
-
-  describe('players', function () {
-    it('should return a static list of players', function () {
-      const expectedPlayers = [
-        new Player({ id: 'al-capone', name: 'Al Capone', cash: 95 }),
-        new Player({ id: 'pat-garret', name: 'Pat Garret', cash: 95 }),
-        new Player({ id: 'wyatt-earp', name: 'Wyatt Earp', cash: 95 })
-      ]
-      const actualPlayers = tableService.players
-      for (let i = 0; i < actualPlayers.length; i++) {
-        const { id: actualId, name: actualName, cash: actualCash } = actualPlayers[i]
-        const { id: expectedId, name: expectedName, cash: expectedCash } = expectedPlayers[i]
-        expect(actualId).to.eql(expectedId)
-        expect(actualName).to.eql(expectedName)
-        expect(actualCash).to.eql(expectedCash)
-      }
-    })
-  })
-})
+    tableService = new TableService(/* options */);
+  });
+  it("should return a static list of players", function () {
+    const expectedPlayers = [
+      new Player({ id: "al-capone", name: "Al Capone", cash: 95 }),
+      new Player({ id: "pat-garret", name: "Pat Garret", cash: 95 }),
+      new Player({ id: "wyatt-earp", name: "Wyatt Earp", cash: 95 }),
+    ];
+    const actualPlayers = tableService.players;
+    for (let i = 0; i < actualPlayers.length; i++) {
+      const {
+        id: actualId,
+        name: actualName,
+        cash: actualCash,
+      } = actualPlayers[i];
+      const {
+        id: expectedId,
+        name: expectedName,
+        cash: expectedCash,
+      } = expectedPlayers[i];
+      expect(actualId).to.eql(expectedId);
+      expect(actualName).to.eql(expectedName);
+      expect(actualCash).to.eql(expectedCash);
+    }
+  });
+  it("should return table state as open when the table is create", () => {
+    expect(tableService.state).to.equal(State.OPEN);
+  });
+});
